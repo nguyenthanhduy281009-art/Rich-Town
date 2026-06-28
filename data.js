9813da67f3539145230a735d7fdb86c08f6335a0
nguyenthanhduy281009-art <nguyenthanhduy281009@gmail.com>
2026-06-28 18:17:25 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,1 @@
-// File: data.js
-export const characters = [
-    { id: 'tanker', name: 'Tanker', level: 10, maxLevel: 20, hp: 80, speed: 40, crit: 10 },
-    { id: 'hunter', name: 'Hunter', level: 15, maxLevel: 25, hp: 50, speed: 90, crit: 40 },
-    { id: 'mage', name: 'Mage', level: 8, maxLevel: 20, hp: 40, speed: 60, crit: 20 }
-];
+
