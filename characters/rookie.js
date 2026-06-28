e2fc8582bb6657129748789d601ff09a8312b4c8
nguyenthanhduy281009-art <nguyenthanhduy281009@gmail.com>
2026-06-28 15:00:42 +0700

    Create rookie.js

---
+++ characters/rookie.js
@@ -0,0 +1,15 @@
+export const rookie = {
+    id: 'rookie',
+    name: 'Rookie',
+    level: 1,
+    // Màu sắc đại diện cho nhân vật (Sẽ thay đổi giao diện dựa trên biến này)
+    color: '#3498db', 
+    glow: 'rgba(52, 152, 219, 0.4)',
+    stats: {
+        hp: 60,
+        speed: 50,
+        crit: 5
+    },
+    skins: ['Default', 'Trained'],
+    description: 'Một chiến binh mới gia nhập, chỉ số ổn định ở mọi mặt.'
+};
