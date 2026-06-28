44363e074f27d2ad049f47854f37d94622f30f0a
nguyenthanhduy281009-art <nguyenthanhduy281009@gmail.com>
2026-06-28 15:44:30 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+export const characters = [
+    {
+        id: 'tanker',
+        name: 'Tanker',
+        lv: 10,
+        stats: { hp: 100, speed: 40, crit: 5 },
+        skins: ['Mặc định', 'Hỏa ngục']
+    },
+    // Thêm tướng mới chỉ cần copy block này
+];
