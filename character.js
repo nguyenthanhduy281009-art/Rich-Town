4e61b8ba4ae64af3314b4893d62efcd97fce1992
nguyenthanhduy281009-art <nguyenthanhduy281009@gmail.com>
2026-06-29 21:17:29 +0700

    Create character.js

---
+++ character.js
@@ -0,0 +1,18 @@
+const STAT_LIMITS = {
+
+    hp: {
+        min: 90,
+        max: 140
+    },
+
+    speed: {
+        min: 130,
+        max: 180
+    },
+
+    crit: {
+        min: 0,
+        max: 30
+    }
+
+};
